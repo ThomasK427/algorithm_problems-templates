c42d7db892f610fbb3799c0a91a180b64aed8dfc
ThomasK427 <1935448858@qq.com>
2018-03-05 15:38:11 +0800

    D accepted

--- problems_classified_by_sources/codeforces/Round#468(div2)/D/D.cpp
+++ problems_classified_by_sources/codeforces/Round#468(div2)/D/D.cpp
@@ -3,9 +3,32 @@ using namespace std;
 typedef long long LL;
 const int INF = 0x3f3f3f3f;
 const LL inf = 0x3f3f3f3f3f3f3f3f;
-const int maxn = 1005;
+const int maxn = 100005;
+
+vector<int> g[maxn];
+int d[maxn];
+
+void dfs(int node, int k){
+    d[k] ^= 1;
+    for(int i = 0; i < g[node].size(); i++){
+        dfs(g[node][i], k + 1);
+    }
+}
 
 int main()
 {
+    int n, p;
+    scanf("%d", &n);
+    for(int i = 2; i <= n; i++){
+        scanf("%d", &p);
+        g[p].push_back(i);
+    }
+    memset(d, 0, sizeof(d));
+    dfs(1, 0);
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += d[i];
+    }
+    printf("%d\n", ans);
     return 0;
 }
