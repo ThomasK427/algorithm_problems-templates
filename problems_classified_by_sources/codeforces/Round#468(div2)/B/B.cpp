adc84964af408ebbe264231e41a62b8a43271d0a
ThomasK427 <1935448858@qq.com>
2018-03-05 15:16:47 +0800

    B accepted

--- problems_classified_by_sources/codeforces/Round#468(div2)/B/B.cpp
+++ problems_classified_by_sources/codeforces/Round#468(div2)/B/B.cpp
@@ -7,5 +7,22 @@ const int maxn = 1005;
 
 int main()
 {
+    int n, a, b;
+    scanf("%d%d%d", &n, &a, &b);
+    a += n - 1;
+    b += n - 1;
+    for(int i = 1; ; i++){
+        a /= 2;
+        b /= 2;
+        if(a == b){
+            if(a == 1){
+                puts("Final!");
+            }
+            else{
+                printf("%d\n", i);
+            }
+            break;
+        }
+    }
     return 0;
 }
