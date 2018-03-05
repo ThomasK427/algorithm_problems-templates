71de3a7505c9315aa7d0cce70841f61b3a2009be
ThomasK427 <1935448858@qq.com>
2018-03-05 15:05:08 +0800

    A accepted

--- problems_classified_by_sources/codeforces/Round#468(div2)/A/A.cpp
+++ problems_classified_by_sources/codeforces/Round#468(div2)/A/A.cpp
@@ -7,5 +7,12 @@ const int maxn = 1005;
 
 int main()
 {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int n = abs(a - b), m = n / 2, ans = m * (m + 1);
+    if(n % 2 == 1){
+        ans += m + 1;
+    }
+    printf("%d\n", ans);
     return 0;
 }
