12d1ed15e588f901a617d9546562b71751ebc9ab
ThomasK427 <1935448858@qq.com>
2018-03-06 10:16:30 +0800

    B accepted

--- problems_classified_by_sources/codeforces/Round#467(div2)/B/B.cpp
+++ problems_classified_by_sources/codeforces/Round#467(div2)/B/B.cpp
@@ -7,5 +7,28 @@ const int maxn = 1005;
 
 int main()
 {
+    int p, y;
+    scanf("%d%d", &p, &y);
+    bool flag = false;
+    while(y > p){
+        bool ok = true;
+        for(int i = 2; i * i <= y && i <= p; i++){
+            if(y % i == 0){
+                ok = false;
+                break;
+            }
+        }
+        if(ok){
+            printf("%d\n", y);
+            flag = true;
+            break;
+        }
+        else{
+            y--;
+        }
+    }
+    if(!flag){
+        puts("-1");
+    }
     return 0;
 }
