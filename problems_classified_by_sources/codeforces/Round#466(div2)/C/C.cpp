2f5bca768437284cd85ffa56d455c83f9c76642d
ThomasK427 <1935448858@qq.com>
2018-03-06 15:14:06 +0800

    C accepted

--- problems_classified_by_sources/codeforces/Round#466(div2)/C/C.cpp
+++ problems_classified_by_sources/codeforces/Round#466(div2)/C/C.cpp
@@ -3,9 +3,49 @@ using namespace std;
 typedef long long LL;
 const int INF = 0x3f3f3f3f;
 const LL inf = 0x3f3f3f3f3f3f3f3f;
-const int maxn = 1005;
+const int maxn = 100005;
+
+char s[maxn], t[maxn];
+set<char> st;
 
 int main()
 {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    scanf("%s", s);
+    for(int i = 0; i < n; i++){
+        st.insert(s[i]);
+    }
+    t[k] = '\0';
+    char minchar = *st.begin(), maxchar = *st.rbegin();
+    if(k > n){
+        for(int i = 0; i < n; i++){
+            t[i] = s[i];
+        }
+        for(int i = n; i < k; i++){
+            t[i] = minchar;
+        }
+    }
+    else{
+        int p = k - 1;
+        while(p >= 0){
+            if(s[p] == maxchar){
+                t[p] = minchar;
+                p--;
+            }
+            else{
+                set<char>::iterator iter = st.find(s[p]);
+                ++iter;
+                t[p] = *iter;
+                p--;
+                break;
+            }
+        }
+        while(p >= 0){
+            t[p] = s[p];
+            p--;
+        }
+    }
+    puts(t);
     return 0;
 }
