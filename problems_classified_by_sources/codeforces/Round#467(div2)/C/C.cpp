47cc25c686f91b87ead09119129de649b3e3ffee
ThomasK427 <1935448858@qq.com>
2018-03-06 10:52:51 +0800

    C accepted

--- problems_classified_by_sources/codeforces/Round#467(div2)/C/C.cpp
+++ problems_classified_by_sources/codeforces/Round#467(div2)/C/C.cpp
@@ -7,5 +7,20 @@ const int maxn = 1005;
 
 int main()
 {
+    LL k, d, t;
+    scanf("%lld%lld%lld", &k, &d, &t);
+    d = (k + d - 1) / d * d;
+    t *= 2;
+    LL p = d + k;
+    double ans = t / p * d;
+    t %= p;
+    if(2 * k >= t){
+        ans += double(t) / 2;
+    }
+    else{
+        t -= 2 * k;
+        ans += k + t;
+    }
+    printf("%.1f\n", ans);
     return 0;
 }
