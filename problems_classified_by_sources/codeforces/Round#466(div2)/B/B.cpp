fadd9e702eaff83eb64667dd47b595131dc631c3
ThomasK427 <1935448858@qq.com>
2018-03-06 15:13:56 +0800

    B accepted

--- problems_classified_by_sources/codeforces/Round#466(div2)/B/B.cpp
+++ problems_classified_by_sources/codeforces/Round#466(div2)/B/B.cpp
@@ -7,5 +7,25 @@ const int maxn = 1005;
 
 int main()
 {
+    LL n, k, A, B;
+    scanf("%lld%lld%lld%lld", &n, &k, &A, &B);
+    LL x = n, ans = 0;
+    if(k == 1){
+        ans = (x - 1) * A;
+    }
+    else{
+        while(x > 1){
+            if(x % k == 0){
+                ans += min((x - x / k) * A, B);
+                x /= k;
+            }
+            else{
+                LL y = max(1LL, x / k * k);
+                ans += (x - y) * A;
+                x = y;
+            }
+        }
+    }
+    printf("%lld\n", ans);
     return 0;
 }
