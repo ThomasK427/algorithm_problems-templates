82a9f0445fb5abe1694956ae9afe545294174ec7
ThomasK427 <1935448858@qq.com>
2018-03-06 14:37:02 +0800

    A accepted

--- problems_classified_by_sources/codeforces/Round#466(div2)/A/A.cpp
+++ problems_classified_by_sources/codeforces/Round#466(div2)/A/A.cpp
@@ -3,9 +3,37 @@ using namespace std;
 typedef long long LL;
 const int INF = 0x3f3f3f3f;
 const LL inf = 0x3f3f3f3f3f3f3f3f;
-const int maxn = 1005;
+const int maxn = 105;
+
+int ar[maxn];
 
 int main()
 {
+    int n, d;
+    scanf("%d%d", &n, &d);
+    for(int i = 0; i < n; i++){
+        scanf("%d", ar + i);
+    }
+    sort(ar, ar + n);
+    int ans = 0;
+    int p = 0, q = 0;
+    while(q < n){
+        while(ar[q] - ar[p] <= d){
+            ans = max(ans, q - p + 1);
+            if(q < n - 1){
+                q++;
+            }
+            else{
+                break;
+            }
+        }
+        if(q < n - 1){
+            p++;
+        }
+        else{
+            break;
+        }
+    }
+    printf("%d\n", n - ans);
     return 0;
 }
