e919a72c88068497a5948143c028994365c1a13b
ThomasK427 <1935448858@qq.com>
2018-03-07 16:20:36 +0800

    C accepted

--- problems_classified_by_sources/codeforces/Educational_Round39/C/C.cpp
+++ problems_classified_by_sources/codeforces/Educational_Round39/C/C.cpp
@@ -3,9 +3,24 @@ using namespace std;
 typedef long long LL;
 const int INF = 0x3f3f3f3f;
 const LL inf = 0x3f3f3f3f3f3f3f3f;
-const int maxn = 1005;
+const int maxn = 100005;
+
+char s[maxn];
+char t[] = "abcdefghijklmnopqrstuvwxyz";
 
 int main()
 {
+    scanf("%s", s);
+    int n = strlen(s);
+    int i = 0, j = 0;
+    while(i < n && j < 26){
+        if(s[i] > t[j]){
+            i++;
+        }
+        else{
+            s[i++] = t[j++];
+        }
+    }
+    puts(j == 26 ? s : "-1");
     return 0;
 }
