42114b18756296169f040b1d536ae2acb3e24df4
ThomasK427 <1935448858@qq.com>
2018-03-07 16:10:45 +0800

    B accepted

--- problems_classified_by_sources/codeforces/Educational_Round39/B/B.cpp
+++ problems_classified_by_sources/codeforces/Educational_Round39/B/B.cpp
@@ -7,5 +7,22 @@ const int maxn = 1005;
 
 int main()
 {
+    LL a, b;
+    scanf("%lld%lld", &a, &b);
+    while(a * b){
+        if(a == 0 && b == 0){
+            break;
+        }
+        else if(a >= 2 * b){
+            a %= (2 * b);
+        }
+        else if(b >= 2 * a){
+            b %= (2 * a);
+        }
+        else{
+            break;
+        }
+    }
+    printf("%lld %lld", a, b);
     return 0;
 }
