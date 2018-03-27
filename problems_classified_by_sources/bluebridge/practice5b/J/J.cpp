ff24e71b9846d3e73c0d3d9b5ac9c15ea73dc361
1935448858@qq.com <1935448858@qq.com>
2018-03-27 10:17:19 +0800

    accepted

--- problems_classified_by_sources/bluebridge/practice5b/J/J.cpp
+++ problems_classified_by_sources/bluebridge/practice5b/J/J.cpp
@@ -3,22 +3,25 @@ using namespace std;
 typedef long long LL;
 const int maxn = 305;
 
-LL dp[maxn][maxn][maxn];
+LL dp[maxn][maxn];
 
-LL dfs(int n, int k, int m){
-    LL& ans = dp[n][k][m];
+LL dfs(int n, int k){
+    LL& ans = dp[n][k];
     if(ans >= 0){
         return ans;
     }
     ans = 0;
-    if(k == 1){
+    if(n == 1 || k == 1){
         ans = 1;
     }
+    else if(n < k){
+        ans = dfs(n, n);
+    }
+    else if(n == k){
+        ans = dfs(n, k - 1) + 1;
+    }
     else{
-        for(int i = m; n - i >= i; i++){
-            ans += dfs(n - i, k - 1, i);
-        }
-        ans++;
+        ans = dfs(n, k - 1) + dfs(n - k, k);
     }
     return ans;
 }
@@ -28,6 +31,6 @@ int main()
     memset(dp, -1, sizeof(dp));
     int n, k;
     scanf("%d%d", &n, &k);
-    printf("%lld\n", dfs(n, k, 1));
+    printf("%lld\n", dfs(n, k));
     return 0;
 }
