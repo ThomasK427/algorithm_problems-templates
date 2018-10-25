0f5732120fdd29b93f93f13ae07d3d99a454a452
1935448858@qq.com <1935448858@qq.com>
2018-10-25 22:32:12 +0800

    delete test

--- clean.py
+++ clean.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def delete_files(path, suffixes):
     relative_paths = os.listdir(path)
@@ -12,4 +13,15 @@ def delete_files(path, suffixes):
         elif os.path.isdir(absolute_path):
             delete_files(absolute_path, suffixes)
 
+def delete_tests(path):
+    relative_paths = os.listdir(path)
+    for relative_path in relative_paths:
+        absolute_path = os.path.join(path, relative_path)
+        if os.path.isdir(absolute_path):
+            if relative_path in ['test', 'Test']:
+                shutil.rmtree(absolute_path)
+            else:
+                delete_tests(absolute_path)
+
 delete_files(os.getcwd(), ['.exe', '.o'])
+delete_tests(os.getcwd())
--- problems_classified_by_sources/codeforces/Round#495(div2)/Test/Test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int maxn = 1005;
-
-int main()
-{
-    return 0;
-}
